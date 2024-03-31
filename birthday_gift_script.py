277761ec60d0504854082c3bf3d814f2865e37f9
laifehacker <165556549+laifehacker@users.noreply.github.com>
2024-03-31 12:19:25 +0200

    Add files via upload

---
+++ birthday_gift_script.py
@@ -0,0 +1,12 @@
+
+def reveal_gift():
+    print("🎉 Congratulations! 🎉")
+    print("\nYou've just unlocked a very special gift:")
+    print("\n✨ A Custom Automation Tailored Just for You! ✨")
+    print("\nWe gaan samen ergens een kop koffie drinken... Dan: we bespreken samen een routine die je graag zou willen automatiseren.")
+    print("\nBijvoorbeeld een automatische e-mailsamenvatting elke ochtend om 7u van al je e-mails, belangrijke e-mails eruit halend inclusief to-do list... Of iets anders, jouw keuze!")
+    print("\nKlaar voor? Prik (print ;-) ) dan nu gelijk een datum met Reinier.")
+    print("\nHappy birthday brother!")
+
+# Execute the function to reveal the gift
+reveal_gift()
